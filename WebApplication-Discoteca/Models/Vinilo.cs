c69e5c5be9cdb8d422c4133bd9b400ab4ae02d9e
Fabián Enrique García <fabianegl79@gmail.com>
2023-07-22 14:54:54 -0300

    Primera entrada

---
+++ WebApplication-Discoteca/Models/Vinilo.cs
@@ -0,0 +1,23 @@
+﻿namespace WebApplication_Discoteca.Models
+{
+    public class Vinilo
+    {
+        private long id;
+        private string titulo_disco;
+        private string nombre_artista;
+        private int ano;
+        private string canciones;
+        private float precio;
+        private string estado;
+        private DateTime fechaAlta;
+
+        public long Id { get => id; set => id = value; }
+        public string Titulo_disco { get => titulo_disco; set => titulo_disco = value; }
+        public string Nombre_artista { get => nombre_artista; set => nombre_artista = value; }
+        public int Ano { get => ano; set => ano = value; }
+        public string Canciones { get => canciones; set => canciones = value; }
+        public float Precio { get => precio; set => precio = value; }
+        public string Estado { get => estado; set => estado = value; }
+        public DateTime FechaAlta { get => fechaAlta; set => fechaAlta=value; }
+    }
+}
